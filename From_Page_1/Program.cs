9d62c865036a70c3ba83f5dc74a3e4c63ba835c4
Zverev 78 <9842905@gmail.com>
2023-03-13 02:10:29 +0600

    1

--- From_Page_1/Program.cs
+++ From_Page_1/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace From_Page_1
-    {
+{
     internal class Program
         {
         static void Main(string[] args)
             {
+            Console.ReadKey();
             }
         }
     }
