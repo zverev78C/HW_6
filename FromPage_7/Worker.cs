76afb372f2721483a00beccd4ed97d762e8cf7e9
Zverev 78 <9842905@gmail.com>
2023-06-16 18:49:09 +0600

    Согласно ТЗ создал класс репозиторий

--- FromPage_7/Worker.cs
+++ FromPage_7/Worker.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace FromPage_7
 {
-    internal class Worker
+    internal struct Worker
     {
+        #region автосвойства
+
+        /// <summary>
+        /// номер записи
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Дата и время добавления записи
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// ФИО работника
+        /// </summary>
+        public string Fio { get; set; }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public DateTime YearsOld { get; private set; }
+
+        /// <summary>
+        /// Рост сотрудника
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Место рождения
+        /// </summary>
+        public string PlaceOfBirth { get; set; }
+        #endregion
+
+
     }
 }
